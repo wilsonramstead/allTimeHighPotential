b95292507098c361838d659dd9ae09799d4567ab
Wilson Ramstead <ramsteadwilson@gmail.com>
2022-11-30 17:26:20 -0800

    added assets

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,12 +28,51 @@ export class HomeComponent implements OnInit {
     this.allAssets = [];
     this.totalCurrentValue = 0;
     this.totalATHvalue = 0;
-    this.pricesDictionary = { 'Apple': [180.96, 0, 'AAPL'], 'atom': [44.45, 0], 'Avalanche': [144.96, 0, 'X:AVAXUSD'], 'Bitcoin': [69044.77, 0, 'X:BTCUSD'], 'Cardano': [3.09, 0, 'X:ADAUSD'], 'Ethereum': [4878.26, 0, 'X:ETHUSD'], 'Facebook': [382.18, 0, 'META'], 'Microsoft': [339.89, 0, 'MSFT'], 'Nvidia': [333.49, 0, 'NVDA'], 'Polygon': [2.92, 0, 'X:MATICUSD'], 'Solana': [259.96, 0, 'X:SOLUSD'], 'Sand': [8.40, 0, 'X:SANDUSD'], 'Tesla': [409.97, 0, 'TSLA'], 'Unity': [201.12, 0, 'U'], 'Vechain': [0.280991, 0, 'X:VETUSD']}
+    this.pricesDictionary = { 
+      'Amazon': [186.57, 0, 'AMZN'], 
+      'Apple': [180.96, 0, 'AAPL'], 
+      'Atom': [44.45, 0, 'X:ATOMUSD'], 
+      'Avalanche': [144.96, 0, 'X:AVAXUSD'],
+      'Bitcoin': [69044.77, 0, 'X:BTCUSD'], 
+      'BITW': [136, 0, 'BITW'], 
+      'Cardano': [3.09, 0, 'X:ADAUSD'], 
+      'Chainlink': [52.70, 0, 'X:LINKUSD'],
+      'Decentraland': [5.85, 0, 'X:MANAUSD'],
+      'DOGE': [0.731578, 0, 'X:DOGEUSD'],
+      'Ethereum': [4878.26, 0, 'X:ETHUSD'],
+      'Facebook': [382.18, 0, 'META'], 
+      'Fantom': [3.46, 0, 'X:FTMUSD'],
+      'Google': [150.71, 0, 'GOOG'], 
+      'Litecoin': [410.26, 0, 'X:LTCUSD'],
+      'Matterport': [33.05, 0, 'MTTR'], 
+      'Microsoft': [339.89, 0, 'MSFT'], 
+      'Nike': [114.18, 0, 'NKE'],
+      'Nvidia': [333.49, 0, 'NVDA'], 
+      'Polkadot': [54.98, 0, 'X:DOTUSD'],
+      'Polygon': [2.92, 0, 'X:MATICUSD'],
+      'QQQ': [404.58, 0, 'QQQ'],
+      'Roblox': [134.72, 0, 'RBLX'], 
+      'Sand': [8.40, 0, 'X:SANDUSD'], 
+      'SHIB': [0.00008616, 0, 'X:SHIBUSD'],
+      'Solana': [259.96, 0, 'X:SOLUSD'], 
+      'SPY': [479.98, 0, 'SPY'],
+      'Starbucks': [122.13, 0, 'SBUX'],
+      'Target': [260.99, 0, 'TGT'],
+      'Tesla': [409.97, 0, 'TSLA'], 
+      'Tron': [0.231673, 0, 'X:TRXUSD'],
+      'Unity': [201.12, 0, 'U'], 
+      'Vechain': [0.280991, 0, 'X:VETUSD'],
+      'VOO': [441.26,0,'VOO'],
+      'XRP': [3.40, 0, 'X:XRPUSD']
+    }
+
     this.newAsset = { 'assetName': '', 'assetAmount': 0, 'todayValue': 0, 'previousATHvalue': 0,}
   }
   addAsset() {
-    this.newAsset.previousALTvalue = parseFloat(String(Number(this.newAsset.assetAmount) * Number(this.pricesDictionary[this.newAsset.assetName][0]))).toFixed(2);
-    this.totalATHvalue = parseFloat(String(Number(this.totalATHvalue) + Number(this.newAsset.previousALTvalue))).toFixed(2);
+    this.newAsset.previousATHvalue = parseFloat(String(Number(this.newAsset.assetAmount) * Number(this.pricesDictionary[this.newAsset.assetName][0]))).toFixed(2);
+    console.log("Number(this.newAsset.assetAmount): ", Number(this.newAsset.assetAmount));
+    console.log("Number(this.pricesDictionary[this.newAsset.assetName][0]): ", Number(this.pricesDictionary[this.newAsset.assetName][0]));
+    this.totalATHvalue = parseFloat(String(Number(this.totalATHvalue) + Number(this.newAsset.previousATHvalue))).toFixed(2);
     this.newAsset.todayAssetPrice = parseFloat(String(this.pricesDictionary[this.newAsset.assetName][1])).toFixed(2);
     this.newAsset.todayValue = parseFloat(String(this.newAsset.assetAmount * this.pricesDictionary[this.newAsset.assetName][1])).toFixed(2);
     this.totalCurrentValue = parseFloat(String(Number(this.totalCurrentValue) + Number(this.newAsset.todayValue))).toFixed(2);
@@ -50,6 +89,8 @@ export class HomeComponent implements OnInit {
   }
   getPriceInfo(selectedOption) {
     this.http.get<any>('https://api.polygon.io/v2/aggs/ticker/' + this.pricesDictionary[selectedOption][2] + '/prev?adjusted=true&apiKey=ZniEFUGxWV3dk4HeEpD5vQoVE2J56qbc', {}).subscribe(data => {
+
+    console.log('data: ', data)
       this.pricesDictionary[selectedOption][1] = data.results[0]['c']; // this is returning the close price of previous day close price, api reference: https://polygon.io/docs/crypto/get_v2_aggs_ticker__cryptoticker__prev
     });
    }
